27a41d25042cfb3ec6c40b25179e2ed53106bb29
byui_student1045 <sco22022@byui.edu>
2023-02-28 19:30:27 -0700

    Updated the JSON

--- chamber/scripts/fetchJSON.js
+++ chamber/scripts/fetchJSON.js
@@ -2,4 +2,17 @@
 
 //Create a varible to hold the JSON file
 
-//const dataFile = ; 
+const dataFile = ;
+
+//Create an async function to get the info, and
+//await a response from the site
+
+async function GetBuisnessInfo()
+{
+    //Use await to wait for things to load in
+    const obtained = await fetch(dataFile);
+    const info = await obtained.json();
+
+    //Test to Display
+    console.table(info.buisnesses);
+};
