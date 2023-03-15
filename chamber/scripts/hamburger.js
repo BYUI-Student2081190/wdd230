88569082a851d5ba17aca9e1291c390e04821f02
byui_student1045 <sco22022@byui.edu>
2023-03-14 18:28:01 -0600

    Fixed home page bleeding, and preped for final

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -1,7 +1,24 @@
 function toggleMenu()
 {
    document.getElementById("navigation").classList.toggle("open");
-}
+   document.getElementById("hamburgerBtn").classList.toggle("open");
+
+   //Test, create an if check to change the inner text of the
+   //Hamburger button.
+
+   //Create an attribute for it.
+   if (x.className == "open")
+   {
+      x.textContent = `X`;
+   }
+
+   else
+   {
+      //Convert the code into a string using JS.
+      //Works.
+      x.innerText = String.fromCharCode(9776);
+   }
+};
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
